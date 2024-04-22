0f242e9d7513b36e5a520eebe368cac97614e17f
Alzhemed <66705985+Alzhemed@users.noreply.github.com>
2024-04-22 11:43:10 -0300

    Create tap_Aula2_Lista.kt

---
+++ tap_Aula2_Lista.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var nomes:ArrayList<String> = ArrayList<String>();
+    //var nomes = ArrayList<String>();
+    
+    var nome1 = "Gabriel";
+    
+    nomes.add(nome1);
+    
+    println(nomes[0]);
+    
+    val nomes2:List<String> = listOf("Jean", "Elise", "Gabriel");
+    
+    println(nomes2[0]);
+    
+}
