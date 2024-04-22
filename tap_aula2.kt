bdb1f31e679f089339613324988b927b070e74ed
Alzhemed <66705985+Alzhemed@users.noreply.github.com>
2024-04-22 09:57:39 -0300

    Update tap_aula2.kt

--- tap_aula2.kt
+++ tap_aula2.kt
@@ -1,6 +1,8 @@
 fun main() {
+    // Constante
     val saudacao: String = "Olá,";
     var nome = "Gabriel";
     var sobrenome = "Silva";
+    // Interpolação
     println("${saudacao} ${nome} ${sobrenome}!");
 }
