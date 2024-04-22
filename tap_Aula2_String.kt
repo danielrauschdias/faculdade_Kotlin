8c0369c41c96aaa25948deddbd91ec7f7b9ac153
Alzhemed <66705985+Alzhemed@users.noreply.github.com>
2024-04-22 10:32:59 -0300

    Update and rename tap_aula2.kt to tap_Aula2_String.kt

---
+++ tap_Aula2_String.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val saudacao: String = "Olá,";
+    var nome = "Gabriel";
+    var sobrenome = "Silva";
+    println("$saudacao $nome $sobrenome!");
+    println("Seu nome tem ${nome.length} letras.");
+    println("E seu sobrenome tem ${sobrenome.length} letras.");
+}
--- tap_aula2.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    // Constante
-    val saudacao: String = "Olá,";
-    var nome = "Gabriel";
-    var sobrenome = "Silva";
-    // Interpolação
-    println("${saudacao} ${nome} ${sobrenome}!");
-}
