7e3ee3d83a1344fc7807d375846f770b87d5e1cc
Daniel Rausch Dias <66705985+Alzhemed@users.noreply.github.com>
2024-04-23 20:39:43 -0300

    Update tap_Aula2_Funcoes5.kt

--- tap_Aula2_Funcoes5.kt
+++ tap_Aula2_Funcoes5.kt
@@ -1,8 +1,8 @@
 //Depois que o tipo é passado na chamada da função, ele substitui todos os
 //T's que estão declarados nela.
 fun <T> toList(vararg args: T): ArrayList<T> {
-    var list = ArrayList<T>();
-    for(i in args) {
+	var list = ArrayList<T>();
+	for(i in args) {
         list.add(i);
     }
   	
@@ -11,5 +11,10 @@ fun <T> toList(vararg args: T): ArrayList<T> {
 
 fun main() {
 	var nums = toList<Int>(1, 2, 3, 4, 5);
-    println(nums);
+	println(nums);
+
+	//Também é possível chamar a função sem informar o tipo, e dessa forma
+	//o compilador identifica automaticamente qual é o tipo.
+	var frutas = toList("Banana", "Morango", "Abacaxi");
+    	println(frutas);
 }
