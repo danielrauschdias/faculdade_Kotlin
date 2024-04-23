c855cff310abad93aaa80c9e2229ae3e508b4e4c
Daniel Rausch Dias <66705985+Alzhemed@users.noreply.github.com>
2024-04-23 09:01:49 -0300

    Update tap_Aula2_Funcoes.kt

--- tap_Aula2_Funcoes.kt
+++ tap_Aula2_Funcoes.kt
@@ -1,15 +1,15 @@
 //A função pode ser simplificada, conforme abaixo, quando possuir somente uma linha.
 fun imprimir(texto: String) = println(texto);
 
-//Função com retorno
-//Caso queira expressar que a função não tem retorno de outra forma, pode ser colocado também
-//depois do ":" o token "Unit".
-//Pode ser colocar a interrogação depois do tipo de um parâmetro para mostrar que ele pode ser "null."
-//Exemplo: x: Double?
-//fun soma(x: Double, y: Double): Double {
-//    return x + y;
-//}
-//Na versão simplificada não pode ser usado o "return", pois o retorno é implícito.
+/*Função com retorno
+*Caso queira expressar que a função não tem retorno de outra forma, pode ser colocado também
+*depois do ":" o token "Unit".
+*Pode ser colocar a interrogação depois do tipo de um parâmetro para mostrar que ele pode ser "null."
+*Exemplo: x: Double?
+*fun soma(x: Double, y: Double): Double {
+*    return x + y;
+*}
+*/Na versão simplificada não pode ser usado o "return", pois o retorno é implícito.
 fun soma(x: Double, y: Double): Double = x + y;
 
 fun main() {
